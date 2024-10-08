996b6fd3bba5bd48332ba5a49b9b60c172df9cdb
Ishani Kalliappan <2007248@apps.nsd.org>
2024-10-08 04:13:30 +0000

    done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,65 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App{
 
-package org.nhs.app;
+/*    public static void main(String args[]){
+        for(int number = 1; number <= 5; number++){
+            for(int counter = 1; counter <= 4; counter++){
+                System.out.print(number);
+            }
+        System.out.println();
+        }
+    }
+*/
 
-// Your code goes here
+    public static void main(String args[]){
+        one();
+        System.out.println();
+        two();
+        System.out.println();
+        three();
+    }
+        
+      
+    
+    public static void one(){
+        for(int number = 1; number <= 5; number++){
+            for(int counter = 1; counter <= number; counter++){
+                System.out.print(number);
+            }
+        System.out.println();
+        }
+    } 
+
+
+
+    public static void two(){
+        for(int number = 1; number <= 5; number++){
+            for(int dot = 5; dot > number; dot--){
+                System.out.print(".");
+            }
+            for(int num = 1; num <= number; num++){
+                System.out.print(number);
+            }
+            System.out.println();
+        }
+    }
+
+
+
+    public static void three(){
+        for(int number = 1; number <= 5; number++){
+                for(int counter = 1; counter < 5 - number + 1; counter++){
+                    System.out.print(".");
+                }
+
+                System.out.print(number);
+
+                for(int counter = 5 - number + 2; counter <= 5; counter++){
+                    System.out.print(".");
+                }
+
+                System.out.println();
+        }
+    }
+
+
+}
